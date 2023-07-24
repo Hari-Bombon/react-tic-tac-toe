8ddba2e54e8b582717db5cf505c488662af728dd
Hari-Bombon <haripriya14022003@gmail.com>
2023-07-24 20:34:41 +0530

    React-tic tac toe

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,13 +1,65 @@
-import './App.css';
-import { Box } from "./components/Box"
+import { useEffect, useState } from 'react'
+import Cell from "./components/Cell"
 
-function App() {
+const App =() => {
+  const [cells, setCells] = useState(["" , "" , "" ,"" , "" , "" ,"" , "" , "" ])
+  const [go, setGo] = useState("circle")
+  const [winningMessage, setWiningMessage] = useState(null)
+
+  const message ="It is now" + go +"'s go."
+  console.log(cells)
+
+
+  const checkScore = () => {
+    const winningCombos = [
+    [0,1,2], [3,4,5], [6,7,8],
+    [0,3,6], [1,4,7],[2,5,8],
+    [0,4,8] ,[2,4,6]
+    ]
+
+    winningCombos.forEach(array => {
+     let circleWins = array.every(cell => cells[cells]==="circle")
+
+     if (circleWins) {
+      setWiningMessage("Circle Wins!")
+      return
+     }
+    })
+  }
+
+  
+  winningCombos.forEach(array => {
+    let crossWins = array.every(cell => cells[cells]==="cross")
+
+    if (crossWins) {
+     setWiningMessage("Cross Wins!")
+     return
+    }
+   })
+  }
+  useEffect(() => {
+  checkScore()
+ }, [cells])
   return (
-    <div className="App">
-      <Box value="X" onClick={null} />
-      
+    <div className="app">
+      <div className="gameboard">
+        {cells.map((cells, index) => 
+        <Cell 
+        key={index} 
+        id={index} 
+        cell={cells}
+        setCells={setCells}
+        go={go}
+        setGo={setGo}
+        cells={cells}
+        winningMessage={winningMessage}
+    />)}
+   {/* <Cell/> */}
+      </div>
+  
+      <p>{winningMessage || message}</p>
     </div>
-  );
-}
+  )
+        
 
 export default App;
